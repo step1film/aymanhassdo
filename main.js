325feefb86d6983914565b64c5fc8bc03817b5dc
step1film <step1film@gmail.com>
2026-05-25 20:31:11 +0200

    Update zone configurations for layout adjustments

--- main.js
+++ main.js
@@ -837,11 +837,11 @@
     // mobilePos: null = zone is skipped on mobile (only 3 zones on mobile).
     const ZONE_CONFIGS = [
       { pos: { top:'10%',  left:'16%'  }, mobilePos: { top:'6%',  left:'5%'  }, delay: 6200,  rotate: -3, maxWidth: '28ch' },
-      { pos: { top:'11%', left:'78%' }, mobilePos: null,                       delay: 15100, rotate:  5, maxWidth: '22ch' },
-      { pos: { top:'35%', left:'14%'  }, mobilePos: { top:'38%', left:'36%' }, delay: 29600,  rotate: -6, maxWidth: '32ch' },
-      { pos: { top:'41%', left:'82%' }, mobilePos: null,                       delay: 39500, rotate:  3, maxWidth: '24ch' },
-      { pos: { top:'63%', left:'20%'  }, mobilePos: { top:'69%', left:'5%'  }, delay: 48400,  rotate:  0, maxWidth: '26ch' },
-      { pos: { top:'67%', left:'73%' }, mobilePos: null,                       delay: 60900, rotate: -7, maxWidth: '30ch' }
+      { pos: { top:'11%', left:'78%' }, mobilePos: null,                       delay: 10100, rotate:  5, maxWidth: '22ch' },
+      { pos: { top:'35%', left:'14%'  }, mobilePos: { top:'28%', left:'26%' }, delay: 20600,  rotate: -6, maxWidth: '32ch' },
+      { pos: { top:'41%', left:'82%' }, mobilePos: null,                       delay: 35500, rotate:  3, maxWidth: '24ch' },
+      { pos: { top:'63%', left:'20%'  }, mobilePos: { top:'69%', left:'5%'  }, delay: 45400,  rotate:  0, maxWidth: '26ch' },
+      { pos: { top:'67%', left:'73%' }, mobilePos: null,                       delay: 57900, rotate: -7, maxWidth: '30ch' }
     ];
 
     function sleep(ms) { return new Promise(r => setTimeout(r, ms)); }
